94c33a237dfb357e4f04692569a7c6198fd19f19
Dewa Angga <devaangga53@gmail.com>
2024-11-17 13:56:16 +0800

    106 done course

--- app/component/main-header/main-header.js
+++ app/component/main-header/main-header.js
@@ -1,26 +1,40 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import classes from "./main-header.module.css";
 import logoImg from "@/assets/logo.png";
 import MainHeaderBackground from "./main-header-background";
+import { usePathname } from "next/navigation";
 
 export default function MainHeader() {
+  const path = usePathname();
   return (
     <>
       <MainHeaderBackground />
       <header className={classes.header}>
         <Link className={classes.logo} href="/">
           <Image src={logoImg} alt="logo" priority />
-          Next Level Food
+          NextLevel Food
         </Link>
 
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <Link
+                href="/meals"
+                className={path.startsWith("/meals") ? classes.active : ""}
+              >
+                Browse Meals
+              </Link>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <Link
+                href="/community"
+                className={path.startsWith("/community") ? classes.active : ""}
+              >
+                Foodies Community
+              </Link>
             </li>
           </ul>
         </nav>
