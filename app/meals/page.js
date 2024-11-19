3b8f8982a1db4cc8e1e9ca4e4def38e705c600b5
Dewa ANgga <devaangga53@gmail.com>
2024-11-19 21:51:14 +0800

    107 done course

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,25 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "../component/meals/meals-grid";
+
 export default function MealsPage() {
   return (
-    <div>
-      <h1>Meals Page</h1>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicius Meals, create{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
