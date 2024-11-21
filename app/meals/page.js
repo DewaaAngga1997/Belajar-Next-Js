f2f2db7fadfb851be5940f89f869191582cf38d8
Dewa Angga <devaangga53@gmail.com>
2024-11-21 23:26:22 +0800

    done 113 course

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,10 +2,13 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "../component/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
-
+  return <MealsGrid meals={meals} />;
+}
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -20,9 +23,10 @@ export default async function MealsPage() {
           <Link href="/meals/share">Share your favorite recipe</Link>
         </p>
       </header>
-      <main className={classes.main}>
-        <MealsGrid meals={meals} />
-      </main>
+      <main className={classes.main}></main>
+      <Suspense fallback={<p className={classes.loading}>Loading ...</p>}>
+        <Meals />
+      </Suspense>
     </>
   );
 }
