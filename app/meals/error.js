5fe80f2e5e7f904d6501db60b5ea27666b151f06
Dewa Angga <devaangga53@gmail.com>
2024-11-23 21:59:44 +0800

    create share form page

--- app/meals/error.js
+++ app/meals/error.js
@@ -4,7 +4,7 @@ export default function Error() {
   return (
     <main className="error">
       <h1>An error occurred!</h1>
-      <p>Could not find requested fecth data...</p>
+      <p>Could not find requested page or meals data...</p>
     </main>
   );
 }
